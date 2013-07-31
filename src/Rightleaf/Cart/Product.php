5119cc6a5271302d609aab30b3e4adf2c4188f5a
Phil Palmieri <phil@rightleaf.com>
2013-07-31 13:38:33 -0400

    product setup

--- src/Rightleaf/Cart/Product.php
+++ src/Rightleaf/Cart/Product.php
@@ -1,21 +1,96 @@
 <?php
-/*
- * This file is part of the Cart package.
- *
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace Rightleaf\Cart;
 
-/**
-* Product class for generating product objects
-*/
-class Product
-{
-	public function __construct(ProductType $type)
-	{
-		$this = $type
-	}
+class ProductException extends \Exception {}
+
+abstract Class Product {
+
+    protected $id = NULL;
+    protected $price = 0;
+    protected $name = 'Unkown Product';
+
+
+    /**
+     * return current price
+     *
+     * @return void
+     * @author
+     **/
+    public function getPrice()
+    {
+        return number_format($this->price);
+    }
+
+    /**
+     * Get the product's name
+     *
+     * @return void
+     * @author
+     **/
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the product's Id
+     *
+     * @return void
+     * @author
+     **/
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the ID
+     *
+     * @return void
+     * @author
+     **/
+    public function setId ($id)
+    {
+        if(strlen(trim($id)) <= 0)
+        {
+            throw new ProductException("Id must contain something");
+        }
+
+        $this->id = $id;
+    }
+
+    /**
+     * Set the price of the product
+     *
+     * @return void
+     * @author
+     **/
+    public function setPrice($price)
+    {
+        if(!is_int($price) || $price < 0)
+        {
+            throw new ProductException("Price must be a positive intiger");
+        }
+
+        $this->price = $price;
+    }
+
+    /**
+     * Set the name
+     *
+     * @return void
+     * @author
+     **/
+    public function setName($name)
+    {
+        if(strlen($name) > 0)
+        {
+            $this->name = $name;
+        }
+
+    }
+
+
+
 }
